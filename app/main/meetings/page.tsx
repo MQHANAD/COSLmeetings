d09c4b20e7fa1557454b2c44e5f9a82047a73a15
Mohanad <s202024100@kfupm.edu.sa>
2025-08-25 13:00:28 +0300

    b

--- app/main/meetings/page.tsx
+++ app/main/meetings/page.tsx
@@ -1,32 +1,36 @@
 import MeetingSection from "../../components/MeetingSection";
-const meetings = [
-  {
-    department: "Oilfield Chemical...",
-    title: "Meeting title",
-    host: "Muhanna...",
-    time: "30/1/2025 3:30PM",
-    type: "Regular",
-    image: "../assets/building.jpg", // Place a dummy image in public folder
-  },
-];
 
-export const meetingData = {
-  pending: meetings,
-  inProgress: meetings,
-  upcoming: [meetings[0], meetings[0]],
-  previous: [meetings[0], meetings[0], meetings[0]],
+export const metadata = {
+  title: "My Meetings",
 };
 
 export default function Home() {
+  const meetings = [
+    {
+      department: "Oilfield Chemical...",
+      title: "Meeting title",
+      host: "Muhanna...",
+      time: "30/1/2025 3:30PM",
+      type: "Regular",
+      image: "/building.jpg", // Must be in public folder
+    },
+  ];
+
+  const meetingData = {
+    pending: meetings,
+    inProgress: meetings,
+    upcoming: [meetings[0], meetings[0]],
+    previous: [meetings[0], meetings[0], meetings[0]],
+  };
+
   return (
-    <div className="min-h-screen p-6 bg-[rgba(255,255,255,0)] text-black">
+    <div className="min-h-screen p-6 bg-transparent text-black">
       <h1 className="text-3xl font-extrabold mb-8">My Meetings</h1>
 
-      <MeetingSection title="Pending meeting invites" meetings={meetingData.pending}  />
+      <MeetingSection title="Pending meeting invites" meetings={meetingData.pending} />
       <MeetingSection title="In Progress" meetings={meetingData.inProgress} />
       <MeetingSection title="Upcoming" meetings={meetingData.upcoming} />
       <MeetingSection title="Previous" meetings={meetingData.previous} />
-
     </div>
   );
 }
