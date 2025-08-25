5d55e10283a75bb0bac1ff629e3351dfe9e421b0
Mohanad <s202024100@kfupm.edu.sa>
2025-08-25 12:55:06 +0300

    a

--- app/main/meetings/page.tsx
+++ app/main/meetings/page.tsx
@@ -1,5 +1,8 @@
-import Head from "next/head";
+// app/main/meetings/page.tsx
+import React from "react";
 import MeetingSection from "../../components/MeetingSection";
+
+// Dummy meetings data
 const meetings = [
   {
     department: "Oilfield Chemical...",
@@ -7,7 +10,7 @@ const meetings = [
     host: "Muhanna...",
     time: "30/1/2025 3:30PM",
     type: "Regular",
-    image: "../assets/building.jpg", // Place a dummy image in public folder
+    image: "/building.jpg", // <-- put dummy image in public folder
   },
 ];
 
@@ -18,19 +21,21 @@ export const meetingData = {
   previous: [meetings[0], meetings[0], meetings[0]],
 };
 
-export default function Home() {
+// Metadata for App Router page
+export const metadata = {
+  title: "My Meetings",
+  description: "View your meetings",
+};
+
+export default function MeetingsPage() {
   return (
-    <div className="min-h-screen p-6 bg-[rgba(255,255,255,0)] text-black">
-      <Head>
-        <title>My Meetings</title>
-      </Head>
+    <div className="min-h-screen p-6 bg-transparent text-black">
       <h1 className="text-3xl font-extrabold mb-8">My Meetings</h1>
 
-      <MeetingSection title="Pending meeting invites" meetings={meetingData.pending}  />
+      <MeetingSection title="Pending meeting invites" meetings={meetingData.pending} />
       <MeetingSection title="In Progress" meetings={meetingData.inProgress} />
       <MeetingSection title="Upcoming" meetings={meetingData.upcoming} />
       <MeetingSection title="Previous" meetings={meetingData.previous} />
-
     </div>
   );
 }
